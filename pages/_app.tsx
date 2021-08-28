0793f2442901c096304ba7b9cb6cb0a06e644fee
Valeriy Yeroshenko <valeriy.yeroshenko@invbox.ru>
2021-08-28 12:42:51 +0300

    f✨ feat: add global styles and configure  fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
+import { FC } from 'react'
 import type { AppProps } from 'next/app'
+import '../global-styles/index.sass'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
     return <Component {...pageProps} />
 }
+
 export default MyApp
