863c35bbe21ff2085de8263f65e8327becf298d9
Valeriy Yeroshenko <valeriy.yeroshenko@invbox.ru>
2021-08-28 14:47:37 +0300

    :sparkles: feat: add lint scripts and configure husky | change progect structure: add src folder

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-import type { AppProps } from 'next/app'
-import '../global-styles/index.sass'
-
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
-}
-
-export default MyApp
